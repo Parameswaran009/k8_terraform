a8db55a81a8ae9556de7a4e2b4791ed10b8e50ee
Parameswaran009 <parameswarankrishnan36@gmail.com>
2023-11-29 16:22:26 +0530

    added tf files

---
+++ data.tf
@@ -0,0 +1,20 @@
+data "aws_ami" "example" {
+  most_recent      = true
+  owners           = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.2.20231113.0-kernel-6.1-x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+data "aws_availability_zones" "azs" {}
