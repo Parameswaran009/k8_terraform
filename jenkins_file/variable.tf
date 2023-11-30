849e4cd34064a2677a859e8ffe2ad9dd2eb8d52f
Parameswaran009 <parameswarankrishnan36@gmail.com>
2023-11-30 16:48:14 +0530

    added tf files

--- jenkins_file/variable.tf
+++ jenkins_file/variable.tf
@@ -3,12 +3,12 @@ variable "vpc_cidr" {
   type        = string
 }
 
-variable "private_subnets" {
+variable "public_subnets" {
   description = "Subnets CIDR"
   type        = list(string)
 }
 
-variable "public_subnets" {
-  description = "Subnets CIDR"
-  type        = list(string)
+variable "instance_type" {
+  description = "Instance Type"
+  type        = string
 }
